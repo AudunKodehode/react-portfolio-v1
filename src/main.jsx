45bd0be5f9f39e5460a82b29bd36c54cb831a320
AudunKodehode <audunkodehode@gmail.com>
2023-10-31 12:36:02 +0100

    *

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { HashRouter as Router } from "react-router-dom";
 
-
 const pathname = location.pathname;
 import './index.css'
 
